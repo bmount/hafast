497750bb3a59e294c451160f74b58c24cdf73277
Brian Mount <bvmount@gmail.com>
2017-12-20 23:50:04 -0800

    Basic "features from accelerated segment test" implementation.
    Defer requiring ring of difference to all have same sign.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ SHELL:=/bin/bash
 
 LIBS = -L./halide/lib -lHalide -lpng -lz -ldl -lpthread -lncurses -ljpeg
 INCLUDES = -I./halide/include -I./halide/tools
-CXXFLAGS = -std=c++14 -O3
+CXXFLAGS = -std=c++14
 
 all:
 	$(CXX) $(CXXFLAGS) $(INCLUDES) hafast.cc -o hafast $(LIBS)
--- hafast.cc
+++ hafast.cc
@@ -4,17 +4,45 @@
 
 namespace ha = Halide;
 
+using Halide::cast;
+using Halide::abs;
+
 int main() {
     ha::Buffer<uint8_t> img = ha::Tools::load_image("images/large.png");
-    ha::Func gray;
+    ha::Func gray("gray"), keypoints("keypoints"), visuals("visuals");
     ha::Var x("x"), y("y"), c("c");
     ha::Expr input = img(x, y);
     input = ha::cast<float>(input);
     input = img(x, y, 0) * 0.299f + img(x, y, 1) * 0.587f + img(x, y, 2) * 0.114f;
-    input = ha::cast<uint8_t>(input);
+    int br = 3; // Destination buffer border, Bresenham radius for ring of 16 surrounding pixels:
+    float tr = 60.f;
     gray(x, y) = input;
 
-    ha::Buffer<uint8_t> output = gray.realize(img.width(), img.height());
+    keypoints(x, y) =
+            cast<float>(abs(gray(x,     y - 3) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x + 1, y - 3) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x + 2, y - 2) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x + 3, y - 1) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x + 3, y)     - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x + 3, y + 1) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x + 2, y + 2) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x + 1, y + 3) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x,     y + 3) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x - 1, y + 3) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x - 2, y + 2) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x - 3, y + 1) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x - 3, y)     - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x - 3, y - 1) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x - 2, y - 2) - gray(x, y)) > tr) +
+            cast<float>(abs(gray(x - 1, y - 3) - gray(x, y)) > tr)
+        >
+            12.f; // 12 of 16 neighbors are sharp transition, value from paper
+
+    visuals(x, y) = ha::cast<uint8_t>(ha::min(keypoints(x, y) * 120.f, 255.f));
+
+    ha::Buffer<uint8_t> output(img.width() - br * 2, img.height() - br * 2);
+    output.set_min(br, br);
+    visuals.realize(output);
 
     ha::Tools::save_image(output, "output.png");
     return 0;
